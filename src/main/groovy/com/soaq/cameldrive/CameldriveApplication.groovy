b41005830395245f17ebe61150244dc35af67832
OmarApQz <omaraparicio07@gmail.com>
2024-07-14 17:57:16 -0600

    Initial commit

---
+++ src/main/groovy/com/soaq/cameldrive/CameldriveApplication.groovy
@@ -0,0 +1,13 @@
+package com.soaq.cameldrive
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CameldriveApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(CameldriveApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/soaq/cameldrive/CameldriveApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.soaq.cameldrive
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CameldriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

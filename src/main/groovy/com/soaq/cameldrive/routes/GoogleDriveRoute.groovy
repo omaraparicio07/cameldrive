abf94d1d777b8bbb2a7911b13c002c07edca30f2
OmarApQz <omaraparicio07@gmail.com>
2024-07-14 19:26:47 -0600

    Add camel googledrive dependencie, and add route file for drive endpoint

---
+++ src/main/groovy/com/soaq/cameldrive/routes/GoogleDriveRoute.groovy
@@ -0,0 +1,13 @@
+package com.soaq.cameldrive.routes
+
+import org.apache.camel.builder.RouteBuilder
+import org.springframework.stereotype.Component
+
+@Component
+class GoogleDriveRoute extends RouteBuilder {
+
+  @Override
+  void configure() throws Exception{
+
+  }
+}

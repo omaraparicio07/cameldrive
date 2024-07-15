f32a4b944c32d0d643c0e4e335ecd7b08d457103
OmarApQz <omaraparicio07@gmail.com>
2024-07-14 19:15:45 -0600

    Add routes package scan in starter class

--- src/main/groovy/com/soaq/cameldrive/CameldriveApplication.groovy
+++ src/main/groovy/com/soaq/cameldrive/CameldriveApplication.groovy
@@ -2,8 +2,10 @@ package com.soaq.cameldrive
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.soaq.cameldrive.routes")
 class CameldriveApplication {
 
 	static void main(String[] args) {

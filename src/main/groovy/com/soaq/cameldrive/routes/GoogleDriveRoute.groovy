943af4c5e4d221d46ed1f942d088c2502e2dfd72
OmarApQz <omaraparicio07@gmail.com>
2024-07-16 09:53:57 -0600

    Add test gd component

--- src/main/groovy/com/soaq/cameldrive/routes/GoogleDriveRoute.groovy
+++ src/main/groovy/com/soaq/cameldrive/routes/GoogleDriveRoute.groovy
@@ -8,6 +8,9 @@ class GoogleDriveRoute extends RouteBuilder {
 
   @Override
   void configure() throws Exception{
-
+    from("google-drive://drive-files/list?clientId=&clientSecret=&delay=10000&scopes=['https://www.googleapis.com/auth/drive']")
+    // from("https://www.googleapis.com/drive/v3/files")
+    // .transform(simple("${body.toString()}"))
+    .log("HI")
   }
 }
